4a6c767d05d7777857b54805905fcd4865533b0b
Wisney Cardeal <wisneycardeal@gmail.com>
2024-11-18 19:45:28 -0300

    Commit inicial aula 1

---
+++ instalike-back/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log("Servidor escutando...");
+});
+
+app.get("/api", (req, res) => {
+    res.status(200).send("Boas vindas a imersão!");
+});
